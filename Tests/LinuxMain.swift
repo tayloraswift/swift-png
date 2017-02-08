0059ae4abb144c2392be7d510a0c10e8870fb41d
Diana Ma <diana@rarestype.com>
2017-02-08 12:11:33 -0600

    fixed a nasty filter bug and added support for *encoding* pngs as well as decoding

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,3 +1,6 @@
 @testable import MaxPNGTests
 
+try skip_png("Tests/taylor.png")
 try read_png("Tests/taylor.png")
+let (png_header, png_scanlines) = try read_png_into_buffer("Tests/taylor.png")
+try write_png("Tests/output.png", png_scanlines, header: png_header)
--- Tests/MaxPNGTests/MaxPNGTests.swift
+++ Tests/MaxPNGTests/MaxPNGTests.swift
@@ -20,24 +20,63 @@ func unix_path(_ path:RelativePath) -> Unixpath
     return expanded_path
 }
 
+func skip_png(_ rpath:RelativePath) throws
+{
+    let path = unix_path(rpath)
+    let _ = try PNGDecoder(path: path, look_for: [])
+}
+
 func read_png(_ rpath:RelativePath) throws
 {
     let path = unix_path(rpath)
-    let png_skip = try PNGDataIterator(path: path, look_for: [])
-
-    let png = try PNGDataIterator(path: path)
+    let png = try PNGDecoder(path: path)
 
     let total_samples = png.header.height*png.header.width*png.header.channels
     var o = 0
     var l = 0
-    while let b = try png.next()
+    while let b = try png.next_scanline()
     {
         o += b.count
         l += 1
-        if (l % 16) == 0
+        if (l % 64) == 0
         {
             print("\(Double(o)/Double(total_samples) * 100) %")
         }
     }
     print("100 %")
+}
+
+func read_png_into_buffer(_ rpath:RelativePath) throws -> (PNGImageHeader, [[UInt8]])
+{
+    let path = unix_path(rpath)
+    let png = try PNGDecoder(path: path)
+
+    let total_samples = png.header.height*png.header.width*png.header.channels
+    var o = 0
+    var l = 0
+    var scanlines:[[UInt8]] = []
+    while let b = try png.next_scanline()
+    {
+        o += b.count
+        l += 1
+        if (l % 64) == 0
+        {
+            print("\(Double(o)/Double(total_samples) * 100) %")
+        }
+        scanlines.append(b)
+    }
+    print("100 %")
+    return (png.header, scanlines)
+}
+
+func write_png(_ rpath:RelativePath, _ scanlines:[[UInt8]], header:PNGImageHeader) throws
+{
+    let path = unix_path(rpath)
+    let png = try PNGEncoder(path: path, header: header)
+    try png.initialize()
+    for scanline in scanlines
+    {
+        try png.add_scanline(scanline)
+    }
+    try png.finish()
 }
