0c38852f0208dba6abf06d2c4d7516cd2b730ce7
taylor swift <kelvin13ma@gmail.com>
2017-04-02 20:45:47 -0500

    basic support for reading interlaced PNG files

--- Tests/MaxPNGTests/MaxPNGTests.swift
+++ Tests/MaxPNGTests/MaxPNGTests.swift
@@ -8,13 +8,15 @@ typealias Unixpath = String
 func unix_path(_ path:RelativePath) -> Unixpath
 {
     guard path.characters.count > 1
-    else {
+    else
+    {
         return path
     }
     let path_i0 = path.startIndex
     let path_i2 = path.index(path_i0, offsetBy: 2)
     var expanded_path:Unixpath = path
-    if path[path.startIndex..<path_i2] == "~/" {
+    if path[path.startIndex..<path_i2] == "~/"
+    {
         expanded_path = String(cString: getenv("HOME")) +
                         path[path.index(path_i0, offsetBy: 1)..<path.endIndex]
     }
@@ -42,7 +44,7 @@ func write_png(_ rpath:RelativePath, _ scanlines:[[UInt8]], header:PNGImageHeade
 }
 
 public
-func process_png(_ rpath:RelativePath, output:RelativePath) throws
+func reencode_png_stream(_ rpath:RelativePath, output:RelativePath) throws
 {
     let path = unix_path(rpath)
     let out = unix_path(output)
@@ -50,17 +52,39 @@ func process_png(_ rpath:RelativePath, output:RelativePath) throws
     let png_encode = try PNGEncoder(path: out, header: png_decode.header)
     try png_encode.initialize()
     let total_scanlines = png_decode.header.height
+    print(png_decode.header)
     var l = 0
     print("0 %")
     while let scanline = try png_decode.next_scanline()
     {
+        print("read line \(l)\n")
         l += 1
         try png_encode.add_scanline(scanline)
-        if (l % 64) == 0
-        {
-            print("\(Double(l)/Double(total_scanlines) * 100) %")
-        }
     }
     try png_encode.finish()
     print("100 %")
+}
+
+public
+func reencode_png(_ rpath:RelativePath, output:RelativePath) throws
+{
+    let path = unix_path(rpath)
+    let png_decode = try PNGDecoder(path: path)
+
+    let total_scanlines = png_decode.header.height
+    print(png_decode.header)
+    var l = 0
+    print("0 %")
+    var scanlines:[[UInt8]] = []
+    scanlines.reserveCapacity(png_decode.header.height)
+    while let scanline = try png_decode.next_scanline()
+    {
+        l += 1
+        scanlines.append(scanline)
+    }
+    print("100 %")
+
+    let out = unix_path(output)
+    //let png_encode = try PNGEncoder(path: out, header: png_decode.header)
+    //try png_encode.initialize()
 }
