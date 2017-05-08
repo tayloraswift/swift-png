778724009330bf5ade33b54615768221b28ea301
taylor swift <kelvin13ma@gmail.com>
2017-05-07 19:56:06 -0500

    add output tests that automatically check content of PNGs for equality

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,42 @@
 import MaxPNGTests
 import MaxPNG
+import Glibc
 
-try decompose_png("Tests/interlace.png", output: "Tests/interlace_reconverted")
-try reencode_png_stream("Tests/taylor.png", output: "Tests/taylor_reconverted.png")
+let interlace_test_in:String = "interlace.png",
+    interlace_test_out:String = "deinterlace.png",
+    image_test_in:String = "taylor.png",
+    image_test_out:String = "taylor_reconverted.png"
 
-try write_png("Tests/output.png", [[0, 0, 0, 255, 255, 255, 255, 0, 255], [255, 255, 255, 0, 0, 0, 0, 255, 0], [120, 120, 255, 150, 120, 255, 180, 120, 255]], header: PNGImageHeader(width: 3, height: 3, bit_depth: 8, color_type: .rgb, interlace: false))
+var passed:Bool = false
+
+try decompose_png("Tests/" + interlace_test_in, output: "Tests/" + interlace_test_out)
+try reencode_png_stream("Tests/" + image_test_in, output: "Tests/" + image_test_out)
+
+try write_png("Tests/output.png", [[0, 0, 0, 255, 255, 255, 255, 0, 255], [255, 255, 255, 0, 0, 0, 0, 255, 0], [120, 120, 255, 150, 120, 255, 180, 120, 255]],
+                header: PNGImageHeader(width: 3, height: 3, bit_depth: 8, color_type: .rgb, interlace: false))
+
+print("Testing images \(image_test_in) == \(image_test_out)")
+if try test_images_identical("Tests/" + image_test_in, "Tests/" + image_test_out)
+{
+    print("images identical")
+    passed = passed && true
+}
+else
+{
+    print("images not identical")
+    passed = false
+}
+
+print("Testing images \(interlace_test_in) == \(interlace_test_out)")
+if try test_images_identical("Tests/" + interlace_test_in, "Tests/" + interlace_test_out)
+{
+    print("images identical")
+    passed = passed && true
+}
+else
+{
+    print("images not identical")
+    passed = false
+}
+
+exit(passed ? 0 : 1)
