c1427c152a49c019d7093d0b5070570090cc065d
taylor swift <kelvin13ma@gmail.com>
2017-05-07 20:08:58 -0500

    tests now take interlacing into account

--- Tests/MaxPNGTests/compare_image.swift
+++ Tests/MaxPNGTests/compare_image.swift
@@ -9,16 +9,33 @@ func test_images_identical(_ img_path1:RelativePath, _ img_path2:RelativePath) t
 
     let png_decode1 = try PNGDecoder(path: path1)
     let png_decode2 = try PNGDecoder(path: path2)
-
-    while let scanline1 = try png_decode1.next_scanline()
+    var png_data1:[[UInt8]] = [],
+        png_data2:[[UInt8]] = []
+    while let scanline = try png_decode1.next_scanline()
     {
-        guard let scanline2 = try png_decode2.next_scanline()
-        else
-        {
-            return false
-        }
+        png_data1.append(scanline)
+    }
+    while let scanline = try png_decode2.next_scanline()
+    {
+        png_data2.append(scanline)
+    }
+    if png_decode1.header.interlace
+    {
+        png_data1 = try deinterlace(scanlines: png_data1, header: png_decode1.header)
+    }
+    if png_decode2.header.interlace
+    {
+        png_data2 = try deinterlace(scanlines: png_data2, header: png_decode2.header)
+    }
 
-        if scanline1 != scanline2
+    guard png_data1.count == png_data2.count
+    else
+    {
+        return false
+    }
+    for (l1, l2):([UInt8], [UInt8]) in zip(png_data1, png_data2)
+    {
+        if l1 != l2
         {
             return false
         }
