2f5ed2b9faf2cbbc2d60757259995732fd3db993
taylor swift <kelvin13ma@gmail.com>
2017-05-14 14:45:33 -0500

    better testing, and equability for RGBA struct

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,8 +2,10 @@
 @testable import MaxPNG
 import Glibc
 
-let interlace_test_in:String = "interlace.png",
+let interlace_test_ref:String = "interlace.rgba",
+    interlace_test_in:String = "interlace.png",
     interlace_test_out:String = "deinterlace.png",
+    image_test_ref:String = "taylor.rgba",
     image_test_in:String = "taylor.png",
     image_test_out:String = "taylor_reconverted.png"
 
@@ -18,8 +20,8 @@ try reencode_png_stream("Tests/" + image_test_in, output: "Tests/" + image_test_
 try write_png("Tests/output.png", [[0, 0, 0, 255, 255, 255, 255, 0, 255], [255, 255, 255, 0, 0, 0, 0, 255, 0], [120, 120, 255, 150, 120, 255, 180, 120, 255]],
                 header: PNGHeader(width: 3, height: 3, bit_depth: 8, color_type: .rgb, interlace: false))
 
-print("Testing images \(image_test_in) == \(image_test_out)")
-if try test_images_identical("Tests/" + image_test_in, "Tests/" + image_test_out)
+print("Testing images \(image_test_ref) == \(image_test_out)")
+if try test_decoded_identical(relative_path_png: "Tests/" + image_test_out, relative_path_rgba: "Tests/" + image_test_ref)
 {
     print("images identical")
     passed = passed && true
@@ -30,8 +32,8 @@ else
     passed = false
 }
 
-print("Testing images \(interlace_test_in) == \(interlace_test_out)")
-if try test_images_identical("Tests/" + interlace_test_in, "Tests/" + interlace_test_out)
+print("Testing images \(interlace_test_ref) == \(interlace_test_out)")
+if try test_decoded_identical(relative_path_png: "Tests/" + interlace_test_out, relative_path_rgba: "Tests/" + interlace_test_ref)
 {
     print("images identical")
     passed = passed && true
--- Tests/MaxPNGTests/decodetests.swift
+++ Tests/MaxPNGTests/decodetests.swift
@@ -1,7 +1,7 @@
 import Glibc
 @testable import MaxPNG
 
-func load_rgb_data<Pixel:UnsignedInteger>(absolute_path:String, n_pixels:Int) -> [RGBA<Pixel>]
+func load_rgba_data<Pixel:UnsignedInteger>(absolute_path:String, n_pixels:Int) -> [RGBA<Pixel>]
 {
     guard let stream:FilePointer = fopen(absolute_path, "rb")
     else
@@ -18,4 +18,21 @@ func load_rgb_data<Pixel:UnsignedInteger>(absolute_path:String, n_pixels:Int) ->
     }
 
     return pixel_data
+}
+
+func test_decoded_identical(relative_path_png:String, relative_path_rgba:String) throws -> Bool
+{
+    let (png_data, png_header):([UInt8], PNGHeader) = try decode_png_contiguous(relative_path: relative_path_png)
+    let rgba_data_png:[RGBA<UInt16>] = rgba32(raw_data: png_data, header: png_header)!.map
+    {
+        let r:UInt16 = UInt16($0.r),
+            g:UInt16 = UInt16($0.g),
+            b:UInt16 = UInt16($0.b),
+            a:UInt16 = UInt16($0.a)
+        return RGBA(r << 8 | r, g << 8 | g, b << 8 | b, a << 8 | a)
+    }
+    let rgba_data_rgba:[RGBA<UInt16>] = load_rgba_data(absolute_path: absolute_unix_path(relative_path_rgba),
+                                                       n_pixels: png_header.width * png_header.height)
+
+    return rgba_data_rgba == rgba_data_png
 }
