fa856e74d1d08c60a4e8b5a61eccfdc3a7f5c645
Diana Ma <diana@rarestype.com>
2017-05-18 12:03:13 -0400

    fixed another interlacing bug, testing system is even more powerful and allows you to select only certain tests to run

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,23 @@
-@testable import MaxPNGTests
-@testable import MaxPNG
+import MaxPNGTests
+import MaxPNG
 import Glibc
+
+var arguments:[String] = CommandLine.arguments
+let _:String = arguments.removeFirst()
+var verbose:Bool = false
+var test_subset:Set<String> = []
+
+for arg in arguments
+{
+    if arg == "-v" || arg == "-verbose" || arg == "--verbose"
+    {
+        verbose = true
+    }
+    else
+    {
+        test_subset.insert(arg)
+    }
+}
 
 try encode_png(path: "Tests/output.png",
                raw_data: [0, 0, 0, 255, 255, 255, 255, 0, 255,
@@ -8,6 +25,6 @@ try encode_png(path: "Tests/output.png",
                           120, 120, 255, 150, 120, 255, 180, 120, 255],
                properties: PNGProperties(width: 3, height: 3, bit_depth: 8, color: .rgb, interlaced: false)!)
 
-run_tests(tests)
+run_tests(tests, verbose: verbose, only_run: test_subset.isEmpty ? nil : test_subset)
 
 exit(0)
