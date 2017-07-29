89e029d7146af232da3ce4c1bfc98b632d2777f7
taylor swift <kelvin13ma@gmail.com>
2017-07-29 01:28:51 -0400

    restore swift test

--- Package.swift
+++ Package.swift
@@ -4,11 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "MaxPNG",
-    products:  [.library(name: "MaxPNG", targets: ["MaxPNG"]),
-                .executable(name: "tests", targets: ["MaxPNGTests"])],
+    products:  [.library(name: "MaxPNG", targets: ["MaxPNG"])],
     targets:   [.target(name: "Zlib", path: "sources/zlib"),
                 .target(name: "MaxPNG", dependencies: ["Zlib"], path: "sources/maxpng"),
-                .target(name: "MaxPNGTests", dependencies: ["MaxPNG"], path: "tests/maxpng")
+                .testTarget(name: "Tests", dependencies: ["MaxPNG"], path: "tests/maxpng")
                ],
     swiftLanguageVersions: [4]
 )
