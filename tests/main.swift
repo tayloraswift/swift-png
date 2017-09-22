74c349dde1198b9895d60f1dc92d1db00ed97a50
Diana Ma <diana@rarestype.com>
2017-09-22 15:36:15 -0500

    make test cases compile and run as explicit executable

---
+++ tests/main.swift
@@ -0,0 +1,23 @@
+import Glibc
+
+do 
+{
+    var arguments:[String] = CommandLine.arguments
+    let _:String = arguments.removeFirst()
+    var verbose:Bool = false
+    var test_subset:Set<String> = []
+
+    for arg in arguments
+    {
+        if arg == "-v" || arg == "-verbose" || arg == "--verbose"
+        {
+            verbose = true
+        }
+        else
+        {
+            test_subset.insert(arg)
+        }
+    }
+
+    exit(run_tests(tests, verbose: verbose, only_run: test_subset.isEmpty ? nil : test_subset))
+}
