01b604c7371934618288604f5d6177fb1c7fcd1d
klossy <kelvin13ma@gmail.com>
2018-09-04 22:25:05 -0500

    finish up paletting and transparency (all decode tests passing!)

--- sources/png/4.2.swift
+++ sources/png/4.2.swift
@@ -31,3 +31,26 @@ extension Array {
         return result
     }
 }
+
+// ...you win this round, swift evolution
+extension BinaryInteger 
+{
+    @inlinable
+    func isMultiple(of other:Self) -> Bool 
+    {
+        // Nothing but zero is a multiple of zero.
+        if other == 0 
+        { 
+            return self == 0 
+        }
+        
+        // Special case to avoid overflow on .min / -1 for signed types.
+        if Self.isSigned && other == -1 
+        { 
+            return true 
+        }
+        
+        // Having handled those special cases, this is safe.
+        return self % other == 0
+    }
+}
