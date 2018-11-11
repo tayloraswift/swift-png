3ca5e098a55e8bce29452e260b395d6678df34ff
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-11 15:34:13 +0100

    Added headings before the configuration file grows.

--- Workspace.swift
+++ Workspace.swift
@@ -1,6 +1,12 @@
 import WorkspaceConfiguration
 
 let configuration:WorkspaceConfiguration = .init()
+
+// ••••••• Proofreading •••••••
+
 configuration.proofreading.rules.remove(.unicode)
 configuration.proofreading.rules.remove(.colonSpacing)
+
+// ••••••• Documentation •••••••
+
 configuration.documentation.localizations = ["en-US"]
