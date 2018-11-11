6687817a55af127fc30292b952ff353fa003026f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-11 16:17:25 +0100

    Configured proofreading.

--- Workspace.swift
+++ Workspace.swift
@@ -4,8 +4,21 @@ let configuration:WorkspaceConfiguration = .init()
 
 // ••••••• Proofreading •••••••
 
+// These are documented at https://sdggiesbrecht.github.io/Workspace/🇨🇦EN/Types/ProofreadingRule.html
+
 configuration.proofreading.rules.remove(.unicode)
 configuration.proofreading.rules.remove(.colonSpacing)
+
+// Disabled because there are violations.
+// For an explanation, see: https://forums.swift.org/t/introducing-documentation-generation-for-swift-packages/15971/8?u=sdggiesbrecht
+configuration.proofreading.rules.remove(.compatibilityCharacters)
+
+// The rules may conflict with the PNG project style:
+// Would require parameter callouts to be grouped.
+configuration.proofreading.rules.remove(.parameterGrouping)
+// Would require repeated line‐style documentation instead of block‐style,
+// which is vulnerable to Xcode’s autoindent destroying semantic Markdown indents.
+configuration.proofreading.rules.remove(.autoindentResilience)
 
 // ••••••• Documentation •••••••
 
