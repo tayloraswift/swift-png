75f6a4697e36c0972b1ffc7813b980ae82b08b30
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-11 23:23:11 +0100

    Fix clock_t for macOS.

--- sources/png/png.swift
+++ sources/png/png.swift
@@ -4426,105 +4426,105 @@ enum PNG
         public static
         func _structuredARGBPremultiplied(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[UInt32] = try? argbPremultiplied(path: path, of: UInt8.self).pixels
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _structuredRGBA(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[RGBA<UInt8>] = try? rgba(path: path, of: UInt8.self).pixels
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _planarRGBA(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[UInt8] = try? rgba(path: path, of: UInt8.self).pixels.planar()
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _interleavedRGBA(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[UInt8] = try? rgba(path: path, of: UInt8.self).pixels.interleaved()
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _structuredVA(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[VA<UInt8>] = try? va(path: path, of: UInt8.self).pixels
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _planarVA(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[UInt8] = try? va(path: path, of: UInt8.self).pixels.planar()
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _interleavedVA(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[UInt8] = try? va(path: path, of: UInt8.self).pixels.interleaved()
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
         public static
         func _structuredV(_ path:String) -> Int
         {
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:[UInt8] = try? v(path: path, of: UInt8.self).pixels
             else
             {
                 fatalError("could not open, read, or decode PNG file '\(path)'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
 
@@ -4545,14 +4545,14 @@ enum PNG
                 fatalError("unreachable")
             }
 
-            let t1:Int = clock()
+            let t1:Int = Int(clock())
             guard let _:Void = try? uncompressed.compress(path: path + ".png")
             else
             {
                 fatalError("could not open, write, or encode PNG file '\(path).png'")
             }
 
-            let t:Int = clock() - t1
+            let t:Int = Int(clock()) - t1
             return t
         }
     }
