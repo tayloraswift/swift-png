6b4e2930c17e835b83e9724fa372fd0a4d36408e
klossy <kelvin13ma@gmail.com>
2019-01-06 22:26:09 -0500

    update readme and clean up workspace files

--- Refresh (Linux).sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-
-set -e
-REPOSITORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${REPOSITORY}"
-gnome-terminal -e "bash --login -c \"source ~/.bashrc; ./Refresh\ \(macOS\).command; exec bash\""
