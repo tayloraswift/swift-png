51db50dd7b09629906803f0c5ad75e2afe0bfea4
kelvin13 <kelvin13ma@gmail.com>
2020-12-22 17:31:32 -0600

    pixel packing for VA and scalar targets, elaborate encode-basic example

--- examples/encode-basic/main.swift
+++ examples/encode-basic/main.swift
@@ -21,10 +21,43 @@ else
     fatalError("failed to open file '\(path).rgba'")
 }
 
-let layout:PNG.Layout           = .init(format: .rgb8(palette: [], fill: nil, key: nil))
-let image:PNG.Data.Rectangular  = .init(size: size, layout: layout, packing: rgba)
+let layout:(rgb:PNG.Layout, v:PNG.Layout) = 
+(
+    rgb:    .init(format: .rgb8(palette: [], fill: nil, key: nil)),
+    v:      .init(format:   .v8(             fill: nil, key: nil))
+)
 
-for level:Int in 0 ... 13 
+do 
 {
-    try image.compress(path: "\(path)@\(level).png", level: level)
+    let image:PNG.Data.Rectangular  = .init(packing: rgba, size: size, layout: layout.rgb)
+    try image.compress(path: "\(path)-color-rgb.png", level: 9)
+    
+    for level:Int in [0, 4, 8, 13]
+    {
+        try image.compress(path: "\(path)-color-rgb@\(level).png", level: level)
+    }
+}
+do 
+{
+    let image:PNG.Data.Rectangular  = .init(packing: rgba, size: size, layout: layout.v)
+    try image.compress(path: "\(path)-color-v.png", level: 9)
+}
+
+let luminance:[UInt8] = rgba.map 
+{
+    let r:Double = .init($0.r), 
+        g:Double = .init($0.g),
+        b:Double = .init($0.b)
+    let l:Double = (0.299 * r * r + 0.587 * g * g + 0.114 * b * b).squareRoot()
+    return .init(max(0, min(l.rounded(), 255)))
+}
+do 
+{
+    let image:PNG.Data.Rectangular  = .init(packing: luminance, size: size, layout: layout.v)
+    try image.compress(path: "\(path)-luminance-v.png", level: 9)
+}
+do 
+{
+    let image:PNG.Data.Rectangular  = .init(packing: luminance, size: size, layout: layout.rgb)
+    try image.compress(path: "\(path)-luminance-rgb.png", level: 9)
 }
