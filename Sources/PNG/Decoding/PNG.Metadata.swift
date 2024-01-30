bcc8a0e76a261e5f64296e96b59c6ce0c1c7aee7
Diana Ma <diana@rarestype.com>
2024-01-30 02:19:16 +0000

    lint last vestiges of the TerminalColors dependency, relocate affected code to the experimental PNGInspection module

--- Sources/PNG/Decoding/PNG.Metadata.swift
+++ Sources/PNG/Decoding/PNG.Metadata.swift
@@ -246,34 +246,3 @@ extension PNG.Metadata
         }
     }
 }
-extension PNG.Metadata:CustomStringConvertible
-{
-    public
-    var description:String
-    {
-        [
-            // singletons
-            [
-                self.time.map               (\.description),
-                self.chromaticity.map       (\.description),
-                self.colorProfile.map       (\.description),
-                self.colorRendering.map     (\.description),
-                self.gamma.map              (\.description),
-                self.histogram.map          (\.description),
-                self.physicalDimensions.map (\.description),
-                self.significantBits.map    (\.description),
-            ].compactMap{ $0 },
-            self.suggestedPalettes.map      (\.description),
-            self.text.map                   (\.description),
-            self.application.map
-            {
-                """
-                <unknown> (\($0.type))
-                {
-                    data        : <\($0.data.count) bytes>
-                }
-                """
-            },
-        ].flatMap{ $0 }.joined(separator: "\n")
-    }
-}
--- Sources/PNG/Parsing/PNG.SuggestedPalette.swift
+++ Sources/PNG/Parsing/PNG.SuggestedPalette.swift
@@ -1,5 +1,3 @@
-import TerminalColors
-
 extension PNG
 {
     /// struct PNG.SuggestedPalette
@@ -243,30 +241,5 @@ extension PNG.SuggestedPalette
             }
             $1 = $0.count
         }
-    }
-}
-extension PNG.SuggestedPalette:CustomStringConvertible
-{
-    public
-    var description:String
-    {
-        let swatches:[String]
-        switch self.entries
-        {
-        case .rgba8(let entries):
-            swatches = entries.enumerated().map{ "        [\($0.0)]: \(String.swatch($0.1.color)) (\($0.1.frequency))" }
-        case .rgba16(let entries):
-            swatches = entries.enumerated().map{ "        [\($0.0)]: \(String.swatch($0.1.color)) (\($0.1.frequency))" }
-        }
-        return """
-        PNG.\(Self.self) (\(PNG.Chunk.sPLT))
-        {
-            name        : '\(self.name)'
-            entries     :
-            [
-        \(swatches.joined(separator: "\n"))
-            ]
-        }
-        """
     }
 }
