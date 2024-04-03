7931e8975e81762a572150d11e424bbf3fe461da
taylorswift <mdianna1989@gmail.com>
2024-04-03 22:11:27 +0000

    add Snippet testing edge cases with system gzip utility

--- Snippets/LZ77/BasicGzip.swift
+++ Snippets/LZ77/BasicGzip.swift
@@ -1,4 +1,5 @@
 import LZ77
+import PNG
 //  snippet.ARCHIVE
 let text:String = "hello barbie"
 
@@ -9,3 +10,14 @@ let archive:[UInt8] = Gzip.archive(
 let utf8:[UInt8] = try Gzip.extract(from: archive[...])
 //  snippet.end
 precondition(utf8.elementsEqual(text.utf8))
+
+let path:String = "Sources/LZ77/docs.docc/GzipCompression"
+//  snippet.EDGE_CASES
+let _:Void? = System.File.Destination.open(path: "\(path)/empty.gz")
+{
+    $0.write(Gzip.archive(bytes: [][...], level: 10))
+}
+let _:Void? = System.File.Destination.open(path: "\(path)/single-byte.gz")
+{
+    $0.write(Gzip.archive(bytes: [0x0A][...], level: 10))
+}
