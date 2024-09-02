bdb8bd034b20a600e7e855316edce4f4d8128255
stackotter <stackotter@stackotter.dev>
2024-09-02 15:34:53 +1000

    Switch back to tayloraswift/swift-hash now that swift-hash PR 18 has been merged

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package:Package = .init(name: "swift-png",
         .executable(name: "decompression-benchmark", targets: ["PNGDecompressionBenchmarks"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stackotter/swift-hash", .upToNextMinor(
+        .package(url: "https://github.com/tayloraswift/swift-hash", .upToNextMinor(
             from: "0.6.3")),
         .package(url: "https://github.com/tayloraswift/swift-grammar", .upToNextMinor(
             from: "0.4.0")),
